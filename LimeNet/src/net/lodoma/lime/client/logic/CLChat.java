c9989da5ab1e43b5bf07bae4a48b1ebf0eb071fe
Lovro Kalinovcic <lovro.kalinovcic@gmail.com>
2014-06-13 11:27:04 +0200

    Bugfix: Client-side Chat packet and handler are added to the pool

--- LimeNet/src/net/lodoma/lime/client/logic/CLChat.java
+++ LimeNet/src/net/lodoma/lime/client/logic/CLChat.java
@@ -2,13 +2,17 @@ package net.lodoma.lime.client.logic;
 
 import net.lodoma.lime.chat.ChatConsole;
 import net.lodoma.lime.chat.ChatManager;
+import net.lodoma.lime.chat.client.CPChatMessage;
+import net.lodoma.lime.chat.client.CPHChatMessage;
 import net.lodoma.lime.client.generic.net.GenericClient;
+import net.lodoma.lime.client.generic.net.packet.ClientPacketPool;
 import net.lodoma.lime.common.net.LogLevel;
 import net.lodoma.lime.util.ThreadHelper;
 
 public class CLChat implements ClientLogic
 {
     private GenericClient client;
+    private ClientPacketPool packetPool;
     
     private ChatConsole chatConsole;
     private ChatManager chatManager;
@@ -28,12 +32,16 @@ public class CLChat implements ClientLogic
     @Override
     public void fetchInit()
     {
+        packetPool = (ClientPacketPool) client.getProperty("packetPool");
         chatManager = (ChatManager) client.getProperty("chatManager");
     }
 
     @Override
     public void generalInit()
     {
+        packetPool.addPacket("Lime::ChatMessage", new CPChatMessage());
+        packetPool.addHandler("Lime::ChatMessage", new CPHChatMessage());
+        
         chatConsole = new ChatConsole(client);
         chatConsole.start();
         
