40e3b01dd9261196c924f73c6bbc76d2571b0b0a
Lovro Kalinovcic <lovro.kalinovcic@gmail.com>
2014-07-08 14:20:04 +0200

    Started implementing Platform

---
+++ src/net/lodoma/lime/world/Platform.java
@@ -0,0 +1,46 @@
+package net.lodoma.lime.world;
+
+import org.jbox2d.dynamics.World;
+
+import net.lodoma.lime.mask.Mask;
+import net.lodoma.lime.util.Vector2;
+
+public class Platform
+{
+    private Mask mask;
+    private Vector2[] points;
+    
+    public Platform(Mask mask, Vector2... points)
+    {
+        assert points.length == 4;
+        
+        this.mask = mask;
+        this.points = points;
+    }
+
+    public Platform(Vector2... points)
+    {
+        this(null, points);
+    }
+    
+    public void setMask(Mask mask)
+    {
+        this.mask = mask;
+    }
+    
+    public void load(World world)
+    {
+        
+    }
+    
+    public void unload(World world)
+    {
+        
+    }
+    
+    public void render()
+    {
+        if(mask != null)
+            mask.render();
+    }
+}
