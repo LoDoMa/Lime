0bf96fe15f7e058fe150509f8510c15674195c5e
Lovro Kalinovcic <lovro.kalinovcic@gmail.com>
2014-07-08 14:35:20 +0200

    Added PhysicsBodyType enumeration

---
+++ src/net/lodoma/lime/physics/PhysicsBodyType.java
@@ -0,0 +1,21 @@
+package net.lodoma.lime.physics;
+
+import org.jbox2d.dynamics.BodyType;
+
+public enum PhysicsBodyType
+{
+    DYNAMIC(BodyType.DYNAMIC),
+    STATIC(BodyType.STATIC);
+    
+    private BodyType engineValue;
+    
+    private PhysicsBodyType(BodyType engineValue)
+    {
+        this.engineValue = engineValue;
+    }
+    
+    public BodyType getEngineValue()
+    {
+        return engineValue;
+    }
+}
