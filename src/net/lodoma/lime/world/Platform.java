412f326193f93d242582037dcebe01d3edc4fbca
Lovro Kalinovcic <lovro.kalinovcic@gmail.com>
2014-07-09 22:05:36 +0200

    [bugfix] Platform now reloads its body after initialization

--- src/net/lodoma/lime/world/Platform.java
+++ src/net/lodoma/lime/world/Platform.java
@@ -19,6 +19,8 @@ public class Platform
         body.setBodyType(PhysicsBodyType.STATIC);
         body.setPolygonShape(vertices);
         body.setPosition(offset);
+        
+        body.reload();
     }
     
     public Platform(Vector2 offset, Vector2... vertices)
