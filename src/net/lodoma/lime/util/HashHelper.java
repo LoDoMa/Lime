b7808754543ff005df5b7d236c46ce3d38b34d2b
Lovro Kalinovcic <lovro.kalinovcic@gmail.com>
2014-07-21 19:06:10 +0200

    Added 'crcFromStream' method to HashHelper

--- src/net/lodoma/lime/util/HashHelper.java
+++ src/net/lodoma/lime/util/HashHelper.java
@@ -1,5 +1,9 @@
 package net.lodoma.lime.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.CRC32;
+
 public class HashHelper
 {
     public static int hash32(String string)
@@ -14,5 +18,15 @@ public class HashHelper
         for (int i = 0; i < len; i++)
             h = 31 * h + string.charAt(i);
         return h;
+    }
+    
+    public static long crcFromStream(InputStream stream) throws IOException
+    {
+        CRC32 crc = new CRC32();
+        byte[] buffer = new byte[1024];
+        int length;
+        while((length = stream.read(buffer)) != -1)
+            crc.update(buffer, 0, length);
+        return crc.getValue();
     }
 }
