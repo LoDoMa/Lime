055fc8eacf7b475bce02fc566ce972b87655b04d
Kalinovcic <lovro.kalinovcic@gmail.com>
2014-09-12 03:52:58 +0200

    Added javadoc for CPChatMessageSend and CPHChatMessageReceive

--- src/net/lodoma/lime/client/io/chat/CPChatMessageSend.java
+++ src/net/lodoma/lime/client/io/chat/CPChatMessageSend.java
@@ -6,11 +6,31 @@ import net.lodoma.lime.client.Client;
 import net.lodoma.lime.client.ClientPacket;
 import net.lodoma.lime.util.HashHelper;
 
+/**
+ * CPChatMessageSend is a ClientPacket sent when
+ * a chat message is sent in the current chat session.
+ * The packet takes the message as a parameter.
+ * 
+ * SPHChatMessageSent is the SPH for this packet.
+ * 
+ * @author Lovro Kalinovčić
+ */
 public class CPChatMessageSend extends ClientPacket
 {
+    /**
+     * Unique name of this packet
+     */
     public static final String NAME = "Lime::ChatMessageSend";
+    
+    /**
+     * 32-bit hash of this packets name;
+     */
     public static final int HASH = HashHelper.hash32(NAME);
     
+    /**
+     * 
+     * @param client - the client that uses this packet
+     */
     public CPChatMessageSend(Client client)
     {
         super(client, HASH, String.class);
--- src/net/lodoma/lime/client/io/chat/CPHChatMessageReceive.java
+++ src/net/lodoma/lime/client/io/chat/CPHChatMessageReceive.java
@@ -7,11 +7,31 @@ import net.lodoma.lime.client.Client;
 import net.lodoma.lime.client.ClientPacketHandler;
 import net.lodoma.lime.util.HashHelper;
 
+/**
+ * CPHChatMessageReceive is a ClientPacketHandler
+ * that handles the SPChatMessageReceive packet.
+ * 
+ * This handler receives a chat message and passes
+ * it to the ChatManager of the current chat session.
+ * 
+ * @author Lovro Kalinovčić
+ */
 public class CPHChatMessageReceive extends ClientPacketHandler
 {
+    /**
+     * Unique name of this handler
+     */
     public static final String NAME = "Lime::ChatMessageReceive";
+    
+    /**
+     * 32-bit hash of this handlers name;
+     */
     public static final int HASH = HashHelper.hash32(NAME);
     
+    /**
+     * 
+     * @param client - the client that uses this handler
+     */
     public CPHChatMessageReceive(Client client)
     {
         super(client);
