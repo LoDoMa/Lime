55f3eed6c9c39592e975b3fff16c150b393f45bd
Kalinovcic <lovro.kalinovcic@gmail.com>
2014-09-12 03:09:20 +0200

    Added Javadoc for ClientConnectionException

--- src/net/lodoma/lime/client/ClientConnectionException.java
+++ src/net/lodoma/lime/client/ClientConnectionException.java
@@ -1,9 +1,17 @@
 package net.lodoma.lime.client;
 
+/**
+ * ClientConnectionException is thrown by Client on failed opening.
+ * @author kalinovcic
+ *
+ */
 public class ClientConnectionException extends Exception
 {
     private static final long serialVersionUID = -5878384965076974066L;
 
+    /**
+     * @param e - cause
+     */
     public ClientConnectionException(Exception e)
     {
         super(e);
