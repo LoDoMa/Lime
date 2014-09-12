2e74fded9db2f3a486ff4ae445e8bfba4e96a77a
Kalinovcic <lovro.kalinovcic@gmail.com>
2014-09-12 03:31:00 +0200

    Added javadoc for CPHNetworkStageChange and CPPresenceResponse

--- src/net/lodoma/lime/client/io/base/CPHNetworkStageChange.java
+++ src/net/lodoma/lime/client/io/base/CPHNetworkStageChange.java
@@ -7,11 +7,31 @@ import net.lodoma.lime.client.ClientPacketHandler;
 import net.lodoma.lime.common.NetStage;
 import net.lodoma.lime.util.HashHelper;
 
+/**
+ * CPHNetworkStageChange is a ClientPacketHandler
+ * that handles the SPNetworkStateChange packet.
+ * 
+ * This handler modifies the "networkStage" property in client
+ * to the received NetStage.
+ * 
+ * @author Lovro Kalinovčić
+ */
 public class CPHNetworkStageChange extends ClientPacketHandler
 {
+    /**
+     * Unique name of this handler
+     */
     public static final String NAME = "Lime::NetworkStageChange";
-    public static final int HASH = HashHelper.hash32(NAME);
     
+    /**
+     * 32-bit hash of this handlers name;
+     */
+    public static final int HASH = HashHelper.hash32(NAME);
+
+    /**
+     * 
+     * @param client - the client that uses this handler
+     */
     public CPHNetworkStageChange(Client client)
     {
         super(client);
