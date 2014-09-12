37ed8e295f4f5ed578233c933a0e3f39420ed026
Kalinovcic <lovro.kalinovcic@gmail.com>
2014-09-12 03:58:54 +0200

    Added javadoc for CPHEntityCorrection, CPHEntityCreation and CPHSetActor

--- src/net/lodoma/lime/client/io/entity/CPHEntityCorrection.java
+++ src/net/lodoma/lime/client/io/entity/CPHEntityCorrection.java
@@ -8,11 +8,31 @@ import net.lodoma.lime.physics.entity.Entity;
 import net.lodoma.lime.physics.entity.EntityCorrector;
 import net.lodoma.lime.util.HashHelper;
 
+/**
+ * CPHEntityCorrection is a ClientPacketHandler
+ * that handles the SPEntityCorrection packet.
+ * 
+ * This handler receives the entityID of an entity and
+ * invokes the "receiveCorrection" method for it.
+ * 
+ * @author Lovro Kalinovčić
+ */
 public class CPHEntityCorrection extends ClientPacketHandler
 {
+    /**
+     * Unique name of this handler
+     */
     public static final String NAME = "Lime::EntityCorrection";
+    
+    /**
+     * 32-bit hash of this handlers name;
+     */
     public static final int HASH = HashHelper.hash32(NAME);
     
+    /**
+     * 
+     * @param client - the client that uses this handler
+     */
     public CPHEntityCorrection(Client client)
     {
         super(client);
--- src/net/lodoma/lime/client/io/entity/CPHEntityCreation.java
+++ src/net/lodoma/lime/client/io/entity/CPHEntityCreation.java
@@ -7,11 +7,31 @@ import net.lodoma.lime.client.ClientPacketHandler;
 import net.lodoma.lime.util.HashHelper;
 import net.lodoma.lime.world.client.ClientsideWorld;
 
+/**
+ * CPHEntityCreation is a ClientPacketHandler
+ * that handles the SPEntityCreation packet.
+ * 
+ * This handler receives the type hash
+ * and creates a new entity of that type.
+ * 
+ * @author Lovro Kalinovčić
+ */
 public class CPHEntityCreation extends ClientPacketHandler
 {
+    /**
+     * Unique name of this handler
+     */
     public static final String NAME = "Lime::EntityCreation";
+    
+    /**
+     * 32-bit hash of this handlers name;
+     */
     public static final int HASH = HashHelper.hash32(NAME);
     
+    /**
+     * 
+     * @param client - the client that uses this handler
+     */
     public CPHEntityCreation(Client client)
     {
         super(client);
