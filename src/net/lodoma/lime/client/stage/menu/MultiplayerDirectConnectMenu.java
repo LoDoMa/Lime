6ef57019f444d1939ac9e309c2fb35adb2f37ef1
Lovro Kalinovčić <lovro.kalinovcic@gmail.com>
2015-03-29 15:07:19 +0200

    MultiplayerDirectConnectMenu now mostly uses RUI

--- src/net/lodoma/lime/client/stage/menu/MultiplayerDirectConnectMenu.java
+++ src/net/lodoma/lime/client/stage/menu/MultiplayerDirectConnectMenu.java
@@ -7,13 +7,11 @@ import net.lodoma.lime.Lime;
 import net.lodoma.lime.client.stage.Stage;
 import net.lodoma.lime.client.stage.game.Game;
 import net.lodoma.lime.client.stage.game.GameMessage;
-import net.lodoma.lime.gui.UICallback;
 import net.lodoma.lime.gui.UITextField;
-import net.lodoma.lime.gui.clean.CleanButton;
-import net.lodoma.lime.gui.clean.CleanText;
 import net.lodoma.lime.gui.clean.CleanTextField;
-import net.lodoma.lime.gui.clean.CleanUI;
 import net.lodoma.lime.input.Input;
+import net.lodoma.lime.rui.RUIEventData;
+import net.lodoma.lime.rui.RUIEventType;
 import net.lodoma.lime.shader.Program;
 import net.lodoma.lime.shader.UniformType;
 import net.lodoma.lime.util.TrueTypeFont;
@@ -21,43 +19,36 @@ import net.lodoma.lime.util.Vector2;
 
 public class MultiplayerDirectConnectMenu extends Stage
 {
-    private class JoinListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            manager.pop();
-            try
-            {
-                manager.push(new Game(InetAddress.getByName(hostField.text.text)));
-            }
-            catch(UnknownHostException e)
-            {
-                Lime.LOGGER.W("Invalid direct connect address; unknown host");
-                
-                manager.push(new GameMessage(e.getClass().getSimpleName() + ": " + e.getMessage()));
-            }
-        }
-    }
-    
-    private class BackListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            manager.pop();
-        }
-    }
-    
     private UITextField hostField;
     
     public MultiplayerDirectConnectMenu()
     {
-        ui.addChild(new CleanText(new Vector2(0.05f, 0.6f), 0.06f, "Direct connect", CleanUI.FOCUS_TEXT_COLOR, TrueTypeFont.ALIGN_LEFT));
-        ui.addChild(new CleanText(new Vector2(0.05f, 0.465f), 0.05f, "Host:", CleanUI.TEXT_COLOR, TrueTypeFont.ALIGN_LEFT));
+        rui.load("DirectConnect");
+        
+        rui.getChildRecursive("body.btnJoin").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE)
+            {
+                manager.pop();
+                try
+                {
+                    manager.push(new Game(InetAddress.getByName(hostField.text.text)));
+                }
+                catch(UnknownHostException e)
+                {
+                    Lime.LOGGER.W("Invalid direct connect address; unknown host");
+                    
+                    manager.push(new GameMessage(e.getClass().getSimpleName() + ": " + e.getMessage()));
+                }
+            }
+        };
+        
+        rui.getChildRecursive("body.btnBack").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE)
+                manager.pop();
+        };
+
+        // TODO: replace old UI text field with new RUI alternative
         ui.addChild(hostField = new CleanTextField(new Vector2(0.15f, 0.44f), new Vector2(0.8f, 0.05f), "localhost", TrueTypeFont.ALIGN_LEFT));
-        ui.addChild(new CleanButton(new Vector2(0.05f, 0.34f), new Vector2(0.4f, 0.05f), "Join", TrueTypeFont.ALIGN_CENTER, new JoinListener()));
-        ui.addChild(new CleanButton(new Vector2(0.05f, 0.26f), new Vector2(0.4f, 0.05f), "Back", TrueTypeFont.ALIGN_CENTER, new BackListener()));
     }
     
     @Override
