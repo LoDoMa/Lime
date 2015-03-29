8d3c58914b61061105b86669c53c4993c176f3fd
Lovro Kalinovčić <lovro.kalinovcic@gmail.com>
2015-03-29 14:38:49 +0200

    MultiplayerMenu now mostly uses RUI

--- src/net/lodoma/lime/client/stage/menu/MultiplayerMenu.java
+++ src/net/lodoma/lime/client/stage/menu/MultiplayerMenu.java
@@ -7,69 +7,54 @@ import java.util.List;
 import net.lodoma.lime.client.ClientBroadcast;
 import net.lodoma.lime.client.stage.Stage;
 import net.lodoma.lime.client.stage.game.Game;
-import net.lodoma.lime.gui.UICallback;
-import net.lodoma.lime.gui.clean.CleanButton;
 import net.lodoma.lime.gui.clean.CleanList;
-import net.lodoma.lime.gui.clean.CleanText;
-import net.lodoma.lime.gui.clean.CleanUI;
 import net.lodoma.lime.input.Input;
+import net.lodoma.lime.rui.RUIEventData;
+import net.lodoma.lime.rui.RUIEventType;
 import net.lodoma.lime.shader.Program;
 import net.lodoma.lime.shader.UniformType;
-import net.lodoma.lime.util.TrueTypeFont;
 import net.lodoma.lime.util.Vector2;
 
 public class MultiplayerMenu extends Stage
 {
-    private class ServerSelectListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            selected = list.getSelectedItem();
-        }
-    } 
+    private CleanList<InetAddress> list;
+    private InetAddress selected;
     
-    private class ConnectListener implements UICallback
+    public MultiplayerMenu()
     {
-        @Override
-        public void call()
-        {
-            if (selected == null)
-                return;
-            manager.push(new Game(selected));
-        }
+        rui.load("MultiplayerMenu");
+        
+        rui.getChildRecursive("body.btnConnect").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE && selected != null)
+                manager.push(new Game(selected));
+        };
+        
+        rui.getChildRecursive("body.btnDirect").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE)
+                manager.push(new MultiplayerDirectConnectMenu());
+        };
+        
+        rui.getChildRecursive("body.btnSearch").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE)
+                searchLAN();
+        };
+        
+        rui.getChildRecursive("body.btnBack").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE)
+                manager.pop();
+        };
+        
+        // TODO: replace old UI list with new RUI alternative
+        ui.addChild(list = new CleanList<InetAddress>(new Vector2(0.05f, 0.75f), new Vector2(0.9f, 0.0f), () -> selected = list.getSelectedItem()));
     }
     
-    private class DirectConnectListener implements UICallback
+    private void searchLAN()
     {
-        @Override
-        public void call()
+        List<InetAddress> addresses = list.getItemList();
+        for (InetAddress address : addresses)
         {
-            manager.push(new MultiplayerDirectConnectMenu());
-        }
-    }
-
-    private class SearchLANListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            List<InetAddress> addresses = list.getItemList();
-            for (InetAddress address : addresses)
+            if (address.toString().startsWith("/"))
             {
-                if (address.toString().startsWith("/"))
-                {
-                    list.removeElement(address, new Comparator<InetAddress>() {
-                        @Override
-                        public int compare(InetAddress o1, InetAddress o2)
-                        {
-                            return o1.toString().equals(o2.toString()) ? 0 : 1;
-                        }
-                    });
-                }
-            }
-            
-            new ClientBroadcast((InetAddress address) -> {
                 list.removeElement(address, new Comparator<InetAddress>() {
                     @Override
                     public int compare(InetAddress o1, InetAddress o2)
@@ -77,61 +62,19 @@ public class MultiplayerMenu extends Stage
                         return o1.toString().equals(o2.toString()) ? 0 : 1;
                     }
                 });
-                list.addElement(address);
+            }
+        }
+        
+        new ClientBroadcast((InetAddress address) -> {
+            list.removeElement(address, new Comparator<InetAddress>() {
+                @Override
+                public int compare(InetAddress o1, InetAddress o2)
+                {
+                    return o1.toString().equals(o2.toString()) ? 0 : 1;
+                }
             });
-        }
-    }
-
-    private class AddServerListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            
-        }
-    }
-
-    private class ModifyServerListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            
-        }
-    }
-
-    private class RemoveServerListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            
-        }
-    }
-    
-    private class BackListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            manager.pop();
-        }
-    }
-
-    private CleanList<InetAddress> list;
-    private InetAddress selected;
-    
-    public MultiplayerMenu()
-    {
-        ui.addChild(new CleanText(new Vector2(0.05f, 0.83f), 0.06f, "Multiplayer", CleanUI.FOCUS_TEXT_COLOR, TrueTypeFont.ALIGN_LEFT));
-        ui.addChild(list = new CleanList<InetAddress>(new Vector2(0.05f, 0.75f), new Vector2(0.9f, 0.0f), new ServerSelectListener()));
-        ui.addChild(new CleanButton(new Vector2(0.05f, 0.20f), new Vector2(0.25f, 0.05f), "Connect", TrueTypeFont.ALIGN_CENTER, new ConnectListener()));
-        ui.addChild(new CleanButton(new Vector2(0.35f, 0.20f), new Vector2(0.25f, 0.05f), "Direct", TrueTypeFont.ALIGN_CENTER, new DirectConnectListener()));
-        ui.addChild(new CleanButton(new Vector2(0.65f, 0.20f), new Vector2(0.25f, 0.05f), "Search LAN", TrueTypeFont.ALIGN_CENTER, new SearchLANListener()));
-        ui.addChild(new CleanButton(new Vector2(0.05f, 0.14f), new Vector2(0.25f, 0.05f), "Add", TrueTypeFont.ALIGN_CENTER, new AddServerListener()));
-        ui.addChild(new CleanButton(new Vector2(0.35f, 0.14f), new Vector2(0.25f, 0.05f), "Modify", TrueTypeFont.ALIGN_CENTER, new ModifyServerListener()));
-        ui.addChild(new CleanButton(new Vector2(0.65f, 0.14f), new Vector2(0.25f, 0.05f), "Remove", TrueTypeFont.ALIGN_CENTER, new RemoveServerListener()));
-        ui.addChild(new CleanButton(new Vector2(0.05f, 0.08f), new Vector2(0.8f, 0.05f), "Back", TrueTypeFont.ALIGN_CENTER, new BackListener()));
+            list.addElement(address);
+        });
     }
     
     @Override
