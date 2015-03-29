22d8a84472bfbbe7abaca73e7c9ba6c45aaa6428
Lovro Kalinovčić <lovro.kalinovcic@gmail.com>
2015-03-29 14:56:19 +0200

    GameMessage now uses RUI

--- src/net/lodoma/lime/client/stage/game/GameMessage.java
+++ src/net/lodoma/lime/client/stage/game/GameMessage.java
@@ -1,29 +1,22 @@
 package net.lodoma.lime.client.stage.game;
 
 import net.lodoma.lime.client.stage.Stage;
-import net.lodoma.lime.gui.UICallback;
-import net.lodoma.lime.gui.clean.CleanButton;
-import net.lodoma.lime.gui.clean.CleanText;
-import net.lodoma.lime.gui.clean.CleanUI;
 import net.lodoma.lime.input.Input;
-import net.lodoma.lime.util.TrueTypeFont;
-import net.lodoma.lime.util.Vector2;
+import net.lodoma.lime.rui.RUIEventData;
+import net.lodoma.lime.rui.RUIEventType;
+import net.lodoma.lime.rui.RUIValue;
 
 public class GameMessage extends Stage
 {
-    private class BackListener implements UICallback
-    {
-        @Override
-        public void call()
-        {
-            manager.pop();
-        }
-    }
-    
     public GameMessage(String text)
     {
-        ui.addChild(new CleanText(new Vector2(0.5f, 0.625f), 0.05f, text, CleanUI.TEXT_COLOR, TrueTypeFont.ALIGN_CENTER));
-        ui.addChild(new CleanButton(new Vector2(0.3f, 0.2f), new Vector2(0.4f, 0.05f), "Back", TrueTypeFont.ALIGN_CENTER, new BackListener()));
+        rui.load("Message");
+        
+        rui.getChildRecursive("body.lblMessage").values.set("default", "text", new RUIValue(text));
+        rui.getChildRecursive("body.btnBack").eventListener = (RUIEventType type, RUIEventData data) -> {
+            if (type == RUIEventType.MOUSE_RELEASE)
+                manager.pop();
+        };
     }
     
     @Override
